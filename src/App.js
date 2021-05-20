9be9c4ceb267fe64868f6c4e10d09f0d0c999736
TTatchell <tim.tatchell24@gmail.com>
2021-05-20 19:52:05 +1000

    upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => this.setState({
+            peopleInSpace: data.people
+        }))
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>'hey'</p>
+            </div>
+        )
+    }
+}
